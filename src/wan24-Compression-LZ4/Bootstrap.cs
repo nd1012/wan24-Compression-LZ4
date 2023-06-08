33fb8fab61928e85999fdaeedee0fa574eab21cd
nd <nd1012@users.noreply.github.com>
2023-06-08 19:00:33 +0200

    Update
    + Added compression profile
    + Updated references

--- src/wan24-Compression-LZ4/Bootstrap.cs
+++ src/wan24-Compression-LZ4/Bootstrap.cs
@@ -12,6 +12,12 @@ namespace wan24.Compression.LZ4
         /// <summary>
         /// Boot
         /// </summary>
-        public static void Boot() => CompressionHelper.Algorithms[Lz4CompressionAlgorithm.ALGORITHM_NAME] = Lz4CompressionAlgorithm.Instance;
+        public static void Boot()
+        {
+            CompressionHelper.Algorithms[Lz4CompressionAlgorithm.ALGORITHM_NAME] = Lz4CompressionAlgorithm.Instance;
+            CompressionProfiles.Registered[Lz4CompressionAlgorithm.PROFILE_LZ4_RAW] = new CompressionOptions()
+                .IncludeNothing()
+                .WithAlgorithm(Lz4CompressionAlgorithm.ALGORITHM_NAME);
+        }
     }
 }
